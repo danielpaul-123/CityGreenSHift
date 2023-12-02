b8b6e3f5327e7b128ebb4c2f389cacacf3b60ce4
danielpaul-123 <danielpaulperinchery07012005@gmail.com>
2023-12-02 19:42:25 +0530

    scroll up button added

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
     document.body.classList.add('fade-in');
 });
+
+document.getElementById('scrollTop').addEventListener('click', function() {
+    const scrollStep = -window.scrollY / 25;
+    const scrollInterval = setInterval(() => {
+       if (window.scrollY !== 0) {
+         window.scrollBy(0, scrollStep);
+       } else {
+         clearInterval(scrollInterval);
+       }
+    }, 10);
+   });
